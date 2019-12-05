31b959d2dc6613af15357c2cc42306442fa2109b
HexSeal <maxfin13@gmail.com>
2019-12-05 13:58:33 -0800

    Tweaked settings for deployment on heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4w&@=p*o7eg54&bu7y1(r9t#i0lli)1og0&$#$n*b9(2r$v=te'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'maxf-django-pollsapp.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
